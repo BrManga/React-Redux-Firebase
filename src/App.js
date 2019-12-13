da944773c112f8e7649c41ba0875f90430059a53
Manga <bora.mng525@gmail.com>
2019-12-13 21:56:37 +0100

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      ELIF
-    </div>
+      <div className="App">
+        <Navbar/>
+      </div>
     </BrowserRouter>
   );
 }
