ba3efa986aa3ecc09121d920f6b660fbf9a9ee20
Manga <bora.mng525@gmail.com>
2019-12-14 22:32:47 +0100

    Navbar change depending on authentication

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -16,6 +16,9 @@ switch (action.type){
     return {
         authError:null
     }
+    case 'SIGNOUT_SUCCESS':
+    console.log('signout success')
+    return state;
     default:
     return state
 }
